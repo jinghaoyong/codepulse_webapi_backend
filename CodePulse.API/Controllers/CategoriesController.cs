1ceb87d6cdf561a0c34d08a7e1ffd8881729ebab
Yong Jing Hao <jinghaoyong@pcomssb.com>
2023-11-28 12:39:32 +0800

    section 5 , part 49 done

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -86,5 +86,37 @@ namespace CodePulse.API.Controllers
 
             return Ok(response);
         }
+
+        //PUT : /api/Categories/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            // Convert DTO to Domain Model
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await categoryRepository.UpdateAsync(category);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain model to DTO
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
