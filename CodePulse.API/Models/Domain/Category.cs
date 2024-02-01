2ee94202966fd955feb1c26184f5f54d5b0636f2
Yong Jing Hao <jinghaoyong@pcomssb.com>
2024-02-01 13:00:44 +0800

    get blogpost

--- CodePulse.API/Models/Domain/Category.cs
+++ CodePulse.API/Models/Domain/Category.cs
@@ -5,5 +5,8 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string UrlHandle { get; set; }
+
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
